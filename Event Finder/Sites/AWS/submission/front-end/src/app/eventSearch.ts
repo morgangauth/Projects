398247327a99a2d73ab0c98f1f916060c408fd99
morgangauth <113058716+morgangauth@users.noreply.github.com>
2023-07-17 15:52:30 -0700

    Removed files

--- Event Finder/Sites/AWS/submission/front-end/src/app/eventSearch.ts
+++
@@ -1,9 +0,0 @@
-export class EventSearch {
-  constructor(
-    public keyword: string,
-    public category: string,
-    public location: string,
-    public autoLocation: boolean,
-    public distance?: number
-  ) {}
-  }
